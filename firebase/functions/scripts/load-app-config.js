07f655133e789cd7663cf2b0925049647002eedd
npomfret <nick-github@snowmonkey.co.uk>
2025-07-13 21:45:10 +0100

    build fix

--- firebase/functions/scripts/load-app-config.js
+++ firebase/functions/scripts/load-app-config.js
@@ -4,7 +4,7 @@ const path = require('path');
 function loadAppConfig() {
   try {
     // Load app-config.json from the root directory
-    const configPath = path.join(__dirname, '../../../../app-config.json');
+    const configPath = path.join(__dirname, '../../../app-config.json');
     if (fs.existsSync(configPath)) {
       const configContent = fs.readFileSync(configPath, 'utf-8');
       return JSON.parse(configContent);
