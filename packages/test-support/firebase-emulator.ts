6b66070edee66d2f9f51d5725f6031169aac9485
Nick Pomfret <nickpomfret@macstudio.local>
2025-08-26 17:25:48 +0100

    removed unused code

--- packages/test-support/firebase-emulator.ts
+++ packages/test-support/firebase-emulator.ts
@@ -2,7 +2,6 @@ import {getFirebaseEmulatorConfig} from './firebase-emulator-config';
 
 const config = getFirebaseEmulatorConfig();
 
-export const FIRESTORE_URL = `http://localhost:${config.firestorePort}`;
 export const FIREBASE_AUTH_URL = `http://localhost:${config.authPort}`;
 export const API_BASE_URL = config.baseUrl;
 export const FIREBASE_API_KEY = config.firebaseApiKey;
