f28019ccdd0985161fd99a8ebf1aca5ff16ebe6a
npomfret <nick-github@snowmonkey.co.uk>
2025-09-17 11:47:21 +0100

    oops - reverted deltion of used method

--- e2e-tests/src/pages/remove-member-modal.page.ts
+++ e2e-tests/src/pages/remove-member-modal.page.ts
@@ -4,18 +4,20 @@ import { BasePage } from './base.page';
 export class RemoveMemberModalPage extends BasePage {
     private dialog: Locator;
     private confirmButton: Locator;
-    private cancelButton: Locator;
 
     constructor(page: Page) {
         super(page);
         this.dialog = this.page.getByTestId('remove-member-dialog');
         this.confirmButton = this.dialog.getByTestId('confirm-button');
-        this.cancelButton = this.dialog.getByTestId('cancel-button');
     }
 
     async confirmRemoveMember(): Promise<void> {
         await expect(this.confirmButton).toBeVisible({ timeout: 2000 });
         await this.clickButton(this.confirmButton, { buttonName: 'Confirm Remove' });
+    }
+
+    async waitForDialogVisible(): Promise<void> {
+        await expect(this.dialog).toBeVisible();
     }
 
 }
