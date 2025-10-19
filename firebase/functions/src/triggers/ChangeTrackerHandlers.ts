79ee009d9d2e919d35c45548fbe9f2dd94bdedeb
npomfret <nick-github@snowmonkey.co.uk>
2025-10-19 15:48:55 +0100

    Refactor change tracking triggers into factory methods
      Move trigger registration logic from change-tracker.ts into ChangeTrackerHandlers
      class as factory methods (getTrack*Changes). This encapsulates trigger configuration
      alongside handler logic, improving testability and reducing coupling between the
      handler class and its registration.
    
      The change-tracker.ts file now simply instantiates handlers and calls factory
      methods to export triggers, eliminating 60+ lines of duplicated configuration
      code.

--- firebase/functions/src/triggers/ChangeTrackerHandlers.ts
+++ firebase/functions/src/triggers/ChangeTrackerHandlers.ts
@@ -1,12 +1,14 @@
 import { DocumentSnapshot } from 'firebase-admin/firestore';
 import { ParamsOf } from 'firebase-functions';
-import { Change, FirestoreEvent } from 'firebase-functions/v2/firestore';
+import { Change, FirestoreEvent, onDocumentWritten } from 'firebase-functions/v2/firestore';
 import { getAuth, getFirestore } from '../firebase';
 import { logger } from '../logger';
 import { ApplicationBuilder } from '../services/ApplicationBuilder';
 import type { IFirestoreReader } from '../services/firestore/IFirestoreReader';
 import type { NotificationService } from '../services/notification-service';
 import { ChangeType } from '../utils/change-detection';
+import { FirestoreCollections } from '../constants';
+import { measureTrigger } from '../monitoring/measure';
 
 export class ChangeTrackerHandlers {
     constructor(
@@ -40,6 +42,19 @@ export class ChangeTrackerHandlers {
         logger.info('group-changed', { id: groupId, groupId, usersNotified: affectedUsers.length });
     };
 
+    getTrackGroupChanges = () =>
+        onDocumentWritten(
+            {
+                document: `${FirestoreCollections.GROUPS}/{groupId}`,
+                region: 'us-central1',
+            },
+            async (event) => {
+                return measureTrigger('trackGroupChanges', async () => {
+                    await this.handleGroupChange(event);
+                });
+            },
+        );
+
     handleExpenseChange = async (
         event: FirestoreEvent<Change<DocumentSnapshot> | undefined, ParamsOf<string>>,
     ) => {
@@ -61,6 +76,19 @@ export class ChangeTrackerHandlers {
 
         logger.info('expense-changed', { id: expenseId, groupId, usersNotified: affectedUsers.length });
     };
+
+    getTrackExpenseChanges = () =>
+        onDocumentWritten(
+            {
+                document: `${FirestoreCollections.EXPENSES}/{expenseId}`,
+                region: 'us-central1',
+            },
+            async (event) => {
+                return measureTrigger('trackExpenseChanges', async () => {
+                    await this.handleExpenseChange(event);
+                });
+            },
+        );
 
     handleSettlementChange = async (
         event: FirestoreEvent<Change<DocumentSnapshot> | undefined, ParamsOf<string>>,
@@ -86,6 +114,19 @@ export class ChangeTrackerHandlers {
         return { groupId, affectedUserCount: affectedUsers.length };
     };
 
+    getTrackSettlementChanges = () =>
+        onDocumentWritten(
+            {
+                document: `${FirestoreCollections.SETTLEMENTS}/{settlementId}`,
+                region: 'us-central1',
+            },
+            async (event) => {
+                return measureTrigger('trackSettlementChanges', async () => {
+                    await this.handleSettlementChange(event);
+                });
+            },
+        );
+
     handleGroupCommentChange = async (
         event: FirestoreEvent<Change<DocumentSnapshot> | undefined, ParamsOf<string>>,
     ) => {
@@ -100,6 +141,19 @@ export class ChangeTrackerHandlers {
 
         logger.info('group-comment-changed', { id: commentId, groupId, usersNotified: affectedUsers.length });
     };
+
+    getTrackGroupCommentChanges = () =>
+        onDocumentWritten(
+            {
+                document: `${FirestoreCollections.GROUPS}/{groupId}/comments/{commentId}`,
+                region: 'us-central1',
+            },
+            async (event) => {
+                return measureTrigger('trackGroupCommentChanges', async () => {
+                    await this.handleGroupCommentChange(event);
+                });
+            },
+        );
 
     handleExpenseCommentChange = async (
         event: FirestoreEvent<Change<DocumentSnapshot> | undefined, ParamsOf<string>>,
@@ -124,6 +178,19 @@ export class ChangeTrackerHandlers {
             usersNotified: affectedUsers.length,
         });
     };
+
+    getTrackExpenseCommentChanges = () =>
+        onDocumentWritten(
+            {
+                document: `${FirestoreCollections.EXPENSES}/{expenseId}/comments/{commentId}`,
+                region: 'us-central1',
+            },
+            async (event) => {
+                return measureTrigger('trackExpenseCommentChanges', async () => {
+                    await this.handleExpenseCommentChange(event);
+                });
+            },
+        );
 
     private extractDataChange(event: FirestoreEvent<Change<DocumentSnapshot> | undefined, ParamsOf<string>>) {
         const before = event.data?.before;
--- firebase/functions/src/triggers/change-tracker.ts
+++ firebase/functions/src/triggers/change-tracker.ts
@@ -1,67 +1,10 @@
-import { onDocumentWritten } from 'firebase-functions/v2/firestore';
 import { getAppBuilder } from '../ApplicationBuilderSingleton';
-import { FirestoreCollections } from '../constants';
-import { measureTrigger } from '../monitoring/measure';
 import { ChangeTrackerHandlers } from './ChangeTrackerHandlers';
 
 const changeTrackerHandlers = ChangeTrackerHandlers.createChangeTrackerHandlers(getAppBuilder());
 
-export const trackGroupChanges = onDocumentWritten(
-    {
-        document: `${FirestoreCollections.GROUPS}/{groupId}`,
-        region: 'us-central1',
-    },
-    async (event) => {
-        return measureTrigger('trackGroupChanges', async () => {
-            await changeTrackerHandlers.handleGroupChange(event);
-        });
-    },
-);
-
-export const trackExpenseChanges = onDocumentWritten(
-    {
-        document: `${FirestoreCollections.EXPENSES}/{expenseId}`,
-        region: 'us-central1',
-    },
-    async (event) => {
-        return measureTrigger('trackExpenseChanges', async () => {
-            await changeTrackerHandlers.handleExpenseChange(event);
-        });
-    },
-);
-
-export const trackSettlementChanges = onDocumentWritten(
-    {
-        document: `${FirestoreCollections.SETTLEMENTS}/{settlementId}`,
-        region: 'us-central1',
-    },
-    async (event) => {
-        return measureTrigger('trackSettlementChanges', async () => {
-            await changeTrackerHandlers.handleSettlementChange(event);
-        });
-    },
-);
-
-export const trackGroupCommentChanges = onDocumentWritten(
-    {
-        document: `${FirestoreCollections.GROUPS}/{groupId}/comments/{commentId}`,
-        region: 'us-central1',
-    },
-    async (event) => {
-        return measureTrigger('trackGroupCommentChanges', async () => {
-            await changeTrackerHandlers.handleGroupCommentChange(event);
-        });
-    },
-);
-
-export const trackExpenseCommentChanges = onDocumentWritten(
-    {
-        document: `${FirestoreCollections.EXPENSES}/{expenseId}/comments/{commentId}`,
-        region: 'us-central1',
-    },
-    async (event) => {
-        return measureTrigger('trackExpenseCommentChanges', async () => {
-            await changeTrackerHandlers.handleExpenseCommentChange(event);
-        });
-    },
-);
+export const trackGroupChanges = changeTrackerHandlers.getTrackGroupChanges();
+export const trackExpenseChanges = changeTrackerHandlers.getTrackExpenseChanges();
+export const trackSettlementChanges = changeTrackerHandlers.getTrackSettlementChanges();
+export const trackGroupCommentChanges = changeTrackerHandlers.getTrackGroupCommentChanges();
+export const trackExpenseCommentChanges = changeTrackerHandlers.getTrackExpenseCommentChanges();
