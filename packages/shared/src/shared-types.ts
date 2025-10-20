d7a2b4136f7f3804a1c84e1233d93b3ad6acf500
npomfret <nick-github@snowmonkey.co.uk>
2025-10-20 17:17:03 +0100

    comment

--- packages/shared/src/shared-types.ts
+++ packages/shared/src/shared-types.ts
@@ -41,6 +41,14 @@ export type ISOString = string;
 export type Amount = string;
 export const ZERO: Amount = '0';
 
+/*
+todo: introduce branded types:
+
+    export type Brand<K, T> = K & { __brand: T };
+
+    export type GroupId = Brand<string, 'GroupId'>;
+ */
+
 export type GroupId = string;
 export type ExpenseId = string;
 export type SettlementId = string;
