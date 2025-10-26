a9930a312cde1684a82ff9932c9cc12e3abfa848
Nick Pomfret <nickpomfret@macstudio.local>
2025-10-26 09:59:28 +0000

    added missing import

--- firebase/scripts/promote-user-to-admin.ts
+++ firebase/scripts/promote-user-to-admin.ts
@@ -12,7 +12,7 @@
  * WARNING: This grants full admin access to the user. Use with caution in production.
  */
 
-import { SystemUserRoles } from '@splitifyd/shared';
+import { SystemUserRoles, UserId } from '@splitifyd/shared';
 import * as admin from 'firebase-admin';
 import { Firestore } from 'firebase-admin/firestore';
 import { FirestoreCollections } from '../functions/src/constants';
