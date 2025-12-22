a66dc56c82a7ba395402eec32bad930107f74a81
npomfret <nick-github@snowmonkey.co.uk>
2025-12-22 08:49:16 +0000

    compile fix

--- packages/test-support/src/builders/AdminTenantRequestBuilder.ts
+++ packages/test-support/src/builders/AdminTenantRequestBuilder.ts
@@ -1,5 +1,5 @@
 import type { AdminUpsertTenantRequest, BrandingMarketingFlags, BrandingTokens, TenantBranding, TenantDomainName, TenantId } from '@billsplit-wl/shared';
-import { toTenantAccentColor, toTenantDefaultFlag, toTenantDomainName, toTenantId, toTenantPrimaryColor, toTenantSecondaryColor } from '@billsplit-wl/shared';
+import { toShowMarketingContentFlag, toShowPricingPageFlag, toTenantAccentColor, toTenantDefaultFlag, toTenantDomainName, toTenantId, toTenantPrimaryColor, toTenantSecondaryColor } from '@billsplit-wl/shared';
 
 /**
  * Builder for creating AdminUpsertTenantRequest for admin API endpoints.
@@ -261,10 +261,17 @@ export class AdminTenantRequestBuilder {
         return this;
     }
 
-    withMarketingFlags(flags: Partial<BrandingMarketingFlags>): this {
+    withMarketingFlags(flags: { showMarketingContent?: boolean; showPricingPage?: boolean }): this {
+        const brandedFlags: Partial<BrandingMarketingFlags> = {};
+        if (flags.showMarketingContent !== undefined) {
+            brandedFlags.showMarketingContent = toShowMarketingContentFlag(flags.showMarketingContent);
+        }
+        if (flags.showPricingPage !== undefined) {
+            brandedFlags.showPricingPage = toShowPricingPageFlag(flags.showPricingPage);
+        }
         this.payload.marketingFlags = {
             ...this.payload.marketingFlags,
-            ...flags,
+            ...brandedFlags,
         };
         return this;
     }
